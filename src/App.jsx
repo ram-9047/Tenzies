0a551336c68a5dbe458af2b8ddd2b7da21b7fb06
Ram <ram@Rams-MacBook-Pro.local>
2025-06-30 16:33:14 +0530

    adds generateNewDices func to generate new dice values and sync it with UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Die from "./components/Die";
 
 function App() {
+  const [diceValue, setDiceValue] = useState(generateNewDices());
+
+  function handleRollDice() {
+    setDiceValue(generateNewDices());
+  }
+
+  function generateNewDices() {
+    let diceValue = new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6));
+    return diceValue;
+  }
+
   return (
     <>
-      <main></main>
+      <main>
+        <div className="dice-container">
+          {diceValue.map((i) => {
+            return <Die value={i} />;
+          })}
+        </div>
+
+        <button className="roll-dice" onClick={handleRollDice}>
+          Roll
+        </button>
+      </main>
     </>
   );
 }
